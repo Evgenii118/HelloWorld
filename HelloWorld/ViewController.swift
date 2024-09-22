5385ffb44a2d2f6dd05ab8c30adada986eb3f496
Ewgeny Izyurov <help@izyapp.ru>
2024-09-22 17:51:24 +0500

    Work with UI. Added constraint for button and Label

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 final class ViewController: UIViewController {
-
+    
+    @IBOutlet var greetingLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        greetingLabel.isHidden.toggle()
+    }
+    
+    @IBAction func greetingButtonDidTap(_ sender: UIButton) {
+        greetingLabel.isHidden.toggle()
+        sender.setTitle(
+            greetingLabel.isHidden ? "Show Greeting" : "Hide Greeting",
+            for: .normal
+        )
+        
+        
+        
         
     }
-
-
 }
 
